5d0344a58c10f6502464c79b0eca10f2826d8d62
greazy <can.think.sometimes@gmail.com>
2012-10-10 19:04:18 +0400

    Console project kraken is created to run kraken library.

---
+++ kraken/kraken_console/main.cpp
@@ -0,0 +1,28 @@
+
+#include "Settings.h"
+#include "PEFileLoader.h"
+
+#include <kraken.h>
+#include <iostream>
+
+using namespace kraken;
+
+int main( int argc, const char** argv )
+{
+  Settings settings( argc, argv );
+
+  if( false == settings.run() )
+  {
+    return EXIT_FAILURE;
+  }
+
+  PEFileLoader fileLoader;
+  cout << "Loading file \"" << settings.path_to_bin() << "\"..." << std::endl;
+  if( fileLoader.load( settings.path_to_bin() ) )
+  {
+    cout << settings.path_to_bin() << " is successfully loaded.";
+  }
+
+  std::getchar();
+  return EXIT_SUCCESS;
+}
